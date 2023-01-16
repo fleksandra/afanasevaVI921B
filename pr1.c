af2f6961bf856154c9162209ef9e598a44d4c24f
fleksandra <60431371+fleksandra@users.noreply.github.com>
2023-01-16 19:13:12 +0300

    Delete pr1.c

--- pr1.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <locale.h> //для поддержки кириллицы
-
-int main(void) {
-    setlocale(LC_ALL, "Russian");
-    printf("Афанасьева Александра Васильевна, группа ВИ921Б");
-    return 0;
-}
